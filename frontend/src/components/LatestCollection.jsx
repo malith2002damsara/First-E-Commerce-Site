306a40a12105db9153ccbf883c67d7289174e63a
malith damsara <malithdamsara87@gmail.com>
2025-07-10 14:38:01 +0530

    nn

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,7 +1,7 @@
 import {useContext, useState, useEffect} from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
-import ProductItem from './Productitem';
+import ProductItem from './ProductItem';
 
 
 const LatestCollection = () => {
