899d4f6fbdc4070e4b09239a956e4dcd13f536e3
malith damsara <malithdamsara87@gmail.com>
2025-08-14 11:36:05 +0530

    cpmplete

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -178,10 +178,10 @@ return (
                             ------------------------Payment Section--------------------------------- */}
 
         <div className="flex gap-3 flex-col lg:flex-row">
-          <div onClick={() => setMethod('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+          {/* <div onClick={() => setMethod('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
             <img src={assets.stripe_logo} alt="" className="h-5 mx-4" />
-          </div>
+          </div> */}
           {/* <div onClick={() => setMethod('razorpay')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
             <img src={assets.razorpay_logo} alt="" className="h-5 mx-4" />
