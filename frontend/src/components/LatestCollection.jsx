c0ae784388208a1553203bb521a3bacbab7088f3
malith damsara <malithdamsara87@gmail.com>
2025-08-14 10:56:55 +0530

    wxvhgwvxw

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -15,9 +15,9 @@ const LatestCollection = () => {
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTION'} />
-        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+        {/* <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
           my name is malith damsraa and i&apos;m ucsc students
-        </p>
+        </p> */}
       </div>
 
       {/* rendering products */}
--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -10,9 +10,9 @@ const onSubmitHandler= (event) =>{
   return (
     <div className='text-center px-5'>
       <p className='text-2xl font-medium text-gray-800'>SUBSCRIBE NOW & GET 20% OFF</p>
-      <p className='text-gray-400 mt-3'>
+      {/* <p className='text-gray-400 mt-3'>
         my name is malith damsara and i&apos;m 22 yeras old
-      </p>
+      </p> */}
       <form onSubmit={onSubmitHandler}  className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="emai" placeholder='Enter your E-mail' required/>
         <button type = 'submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
